a2d1413ff249b5ee79700ea65807184fd901701e
Vrushank Gunjur <vrushankg@hotmail.com>
2023-12-04 21:40:24 -0800

    cleaned personality generator

--- gen_personalities.py
+++ gen_personalities.py
@@ -1,5 +1,4 @@
 import os
-import random
 import json
 import numpy as np
 
@@ -10,21 +9,21 @@ def main():
     [playfulness, chase-proneness, curiosity, sociability, aggressiveness, shyness]
     """
 
-    # first, get all of the dog IDs in the dataset
-
     dog_ids = []
 
     personalities = {}
 
+    # get all dog ids in dataset
     for folder in [TARGET_DATASET_PATH + f'/{p}/Images'for p in ['test', 'train', 'valid']]:
         for breed in os.listdir(folder):
             if breed != '.DS_Store':
                 dog_ids += [i[:-4] for i in os.listdir(folder + f'/{breed}')]
 
-    rng = np.random.default_rng()
+    # generate personalities
     for dog_id in dog_ids:
         personalities[dog_id] = [round(min(max(np.random.normal(5, 2), 1), 10)) for _ in range(6)]
     
+    # write data
     with open('personalities.json', '+w') as outfile:
         json.dump(personalities, outfile)
 
